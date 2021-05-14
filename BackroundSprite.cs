86b6fb341f514f992126718a680f3baddce87839
Caramelcatt <62495268+Caramelcatt@users.noreply.github.com>
2021-05-14 20:25:30 +0800

    Add files via upload

---
+++ BackroundSprite.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Text;
+
+namespace Game4
+{
+    class BackroundSprite
+    {
+        Vector2 distance;
+        Texture2D image;
+        Vector2 firstpos;
+        public Vector2 position;
+        Color colorz = new Color(255, 168, 210);
+        const float _speed = 0.5f;
+        //  new Vector2(((backroundsymbols_position.X) + backroundsymbols_velocity.X), backroundsymbols_velocity.Y), backroundsymbols_color)
+        //  backroundsymbols_position = new Vector2(-119, 62);
+        public BackroundSprite(Vector2 position)
+        {
+            this.position =  position;
+        }
+        public void Initialize()
+        {
+            firstpos = position;
+            distance = new Vector2((firstpos.X + 119) + (119 + 720), (firstpos.Y + 62) + (62 + 720));
+        }
+        public void Update()
+        {
+            if (position.Y >= distance.Y)
+            {
+                position = new Vector2(firstpos.X, firstpos.Y);
+            }
+        }
+        public void LoadContent(ContentManager Content)
+        {
+            image = Content.Load<Texture2D>("SWOONBATTLE/francis_swoon_btl_4itemsymbol");
+        }
+        public void Draw(SpriteBatch _spriteBatch)
+        {
+            _spriteBatch.Draw(image, position, colorz);
+        }
+    }
+}
---
+++ Game1.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game4
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        List<BackroundSprite> BackroundSpriteRow = new List<BackroundSprite>();
+        List<List<BackroundSprite>> BackroundSpriteRows = new List<List<BackroundSprite>>();
+        BackroundSprite BackroundSymbol;
+        Color backroundsymbols_color = new Color(255, 168, 210);
+        Song song;
+        public Vector2 dimentions_of_img = new Vector2(119, 62);
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            _graphics.IsFullScreen = false;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.PreferredBackBufferWidth = 720;
+        }
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            var xadd = 119f;
+            for (int i = 0; i < 21; i++)
+            {
+                BackroundSpriteRow.Add(new BackroundSprite(new Vector2(dimentions_of_img.X += xadd, dimentions_of_img.Y)));
+                xadd += 119f;
+            }
+            for (int i = 0; i < 21; i++)
+            {
+                foreach (var newBackroundSprite in BackroundSpriteRow)
+                {
+                    newBackroundSprite.position.Y += 62f;
+                }
+                BackroundSpriteRows.Add(BackroundSpriteRow);
+            }
+            foreach (List<BackroundSprite> BackroundRow in BackroundSpriteRows)
+            {
+                foreach (BackroundSprite backroundSprite in BackroundRow)
+                {
+                    backroundSprite.Initialize();
+                }
+            }
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            //BackroundSprite(Texture2D newtexture, Vector2 newvector2_startingpos, Vector2 newvector2_velcity, int newincrement, Color newcolor)
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            song = Content.Load<Song>("SWOONBATTLE/2021-05-01 02-13-54");
+            foreach (List<BackroundSprite> BackroundRow in BackroundSpriteRows)
+            {
+                foreach (BackroundSprite backroundSprite in BackroundRow)
+                {
+                    backroundSprite.LoadContent(this.Content);
+                }
+            }
+            // TODO: use this.Content to load your game content here
+            //backroundsymbols dimentions are 119 62
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+            foreach (List<BackroundSprite> BackroundRow in BackroundSpriteRows)
+            {
+                foreach (BackroundSprite backroundSprite in BackroundRow)
+                {
+                    backroundSprite.Update();
+                }
+            }
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(new Color(138, 61, 98));
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            foreach (List<BackroundSprite> BackroundRow in BackroundSpriteRows)
+            {
+                foreach (BackroundSprite backroundSprite in BackroundRow)
+                {
+                    backroundSprite.Draw(_spriteBatch);
+                }
+            }
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
